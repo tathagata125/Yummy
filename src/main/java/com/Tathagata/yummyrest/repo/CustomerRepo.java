92f89e285adf382e164e7787cf2db9047caf3384
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2024-11-19 20:15:08 +0530

    Delete src directory

--- src/main/java/com/Tathagata/yummyrest/repo/CustomerRepo.java
+++
@@ -1,8 +0,0 @@
-package com.Tathagata.yummyrest.repo;
-
-import com.Tathagata.yummyrest.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepo extends JpaRepository<Customer, Long> {
-
-}
